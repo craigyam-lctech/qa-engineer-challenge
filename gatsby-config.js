f5bfbf375006a2760208db7c7ae20966374976a4
Craig Yam <CraigYam@lanecrawford.com>
2024-12-30 12:06:47 +0800

    Initial Setup

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://hypercubex.github.io/test-site-qa/`,
+  },
+  plugins: [
+    `gatsby-plugin-provide-react`,
+  ],
+  pathPrefix: "/Lane-Crawford-Tech",
+}
