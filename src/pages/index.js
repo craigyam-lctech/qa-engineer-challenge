e407c454af3b90c1c4fce4cab7b20bd0d5caa0e6
Craig Yam <CraigYam@lanecrawford.com>
2024-12-30 12:22:48 +0800

    Update content for index,js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import '../styles/index.css'
 const IndexPage = () => {
   return (
     <main>
-      <title>QA Engineer challenge</title>
+      <title>QA Engineer Challenge</title>
       <div
         id='main-content'
         data-testid='main-content-container'>
@@ -33,34 +33,34 @@ const IndexPage = () => {
             here are the general instructions:
           </p>
           <ol>
-            <li>relax, this challenge is expected to consume less than <u>8 hours</u></li>
-            <li>please write tests cases and automated test scripts for this site (<b>this page</b> and the <Link data-testid='main-instruction-products-link' to='/products'>Product Page</Link>, mainly)</li>
-            <li>automated test scripts with selenium/cypress/jest/Puppeteer or any test frameworks that we can run your tests</li>
-            <li>provide some simple instructions on how can we run the tests</li>
-            <li>a brief explanation/assumptions or what do the tests cover, for example, what success cases and failed cases are considered</li>
+            <li>Relax, this challenge is expected to consume less than <u>8 hours</u></li>
+            <li>Please write tests cases and automated test scripts for this site (<b>this page</b> and the <Link data-testid='main-instruction-products-link' to='/products'>Product Page</Link>, mainly)</li>
+            <li>Automated test scripts with selenium/cypress/jest/Puppeteer or any test frameworks that we can run your tests</li>
+            <li>Please provide instructions to run the tests</li>
+            <li>Please provide a brief explanation/assumptions on test coverage, e.g. list of pass and fail cases with validation criteria</li>
           </ol>
 
           <p style={{ marginTop: 20 }}>we would like to ask you to pick one of the following methods to send us your work:</p>
           <ul>
-            <li>compress the source file and email to <a href='mailto:betalabstech@lcjgroup.com'>betalabstech@lcjgroup.com</a></li>
+            <li>Compress the source file and email to <a href='mailto:betalabstech@lcjgroup.com'>betalabstech@lcjgroup.com</a></li>
             <li style={{ marginTop: 20 }}>
               <ol>
-                <li>create a private repository on github</li>
-                <li>invite <a data-testid='main-instruction-collaborator' href='https://github.com/lcjg-betalabstech'>lcjg-betalabstech</a> as a collaborator once you are happy to show your work</li>
+                <li>Create a private repository on github</li>
+                <li>Invite <a data-testid='main-instruction-collaborator' href='https://github.com/lcjg-betalabstech'>lcjg-betalabstech</a> as a collaborator once you are happy to show your work</li>
               </ol>
             </li>
             <li style={{ marginTop: 20 }}>
               <ol>
-                <li>compress all your works</li>
-                <li>upload it to a cloud service, e.g. Dropbox/Google Drive</li>
-                <li>send us the link to get the compressed file</li>
+                <li>Compress all your works</li>
+                <li>Upload it to a cloud service, e.g. Dropbox/Google Drive</li>
+                <li>Send us the link to get the compressed file</li>
               </ol>
             </li>
           </ul>
 
           <div data-testid='main-instruction-remarks'>
-            <div>* for this page, a simple snapshot test should be good enough,</div>
-            <div style={{ fontSize: 12 }}>of course, you are welcome to add more</div>
+            <div>* For this page, a simple snapshot test should be good enough,</div>
+            <div style={{ fontSize: 12 }}>In Addition, you are welcome to add more</div>
             <div>** It will great if you can suggest what improvements we can make</div>
           </div>
         </div>
